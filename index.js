5f39633e0458edc910c76c1f814e6c98f206536e
Robert Clarke <r.jason.clarke@gmail.com>
2021-03-08 03:21:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Your code here
 function createEmployeerecord(record) {
   const employeeRecord = {
+    firstName: record[0],
+    familyName: record[1],
+    title: record[2],
+    payPerHour: record[3],
+    timeInEvents: [],
+    timeOutEvents: []
 
   };
   return employeeRecord;
