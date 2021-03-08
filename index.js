c5a19f26365fba876999918123075a7383117076
Robert Clarke <r.jason.clarke@gmail.com>
2021-03-08 03:22:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,44 @@ function createEmployeerecord(record) {
   };
   return employeeRecord;
 }
+
+function createEmployeeRecords(employeeRecordsArray) {
+  return employeeRecordsArray.map(array => {
+    return createEmployeeRecord(array);
+  });
+}
+
+function createTimeInEvent(employeeRecord, dateStamp) {
+  let stamp = dateStamp.split(' ');
+  let date = stamp[0], hour = stamp[1]; 
+  // let [date, hour] = dateStamp.split(' ');
+  employeeRecord.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date: date
+  });
+  return employeeRecord;
+}
+
+function createTimeOutEvent(employeeRecord, dateStamp) {
+  let [date, hour] = dateStamp.split(' ');
+
+  employeeRecord.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date: date
+  });
+  return employeeRecord;
+}
+
+function hoursWorkedOnDate(employeeRecord, date) {
+  const timeInEvent = employeeRecord.timeInEvents.find(event => {
+    return event.date === date;
+  });
+
+  const timeOutEvent = employeeRecord.timeOutEvents.find(event => {
+    return event.date === date;
+  });
+
+  return (timeOutEvent.hour - timeInEvent.hour) / 100;
+}
